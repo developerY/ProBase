9089f9eabdc7a69619a9abc6372375fcb8b11cb2
developer <developer@ylabz.com>
2026-03-28 11:44:26 -0700

    chore(photodo:mobile): update versionName to 0.1
    This commit updates the application's version name in the mobile module's build configuration.
    
    - **`build.gradle.kts`**:
        - Updated `versionName` from "1.0" to "0.1".

--- applications/photodo/apps/mobile/build.gradle.kts
+++ applications/photodo/apps/mobile/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     defaultConfig {
         applicationId = "com.zoewave.probase.photodo"
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
